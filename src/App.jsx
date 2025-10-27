e3e292ff63452b6399b62aac04b39d19a52236ce
Victor Caro <sbigtheo@gmail.com>
2025-10-27 19:07:11 -0300

    GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Login from './components/Login';
+import SalarySimulator from "./components/SalarySimulator";
+import MaxNumber from "./components/MaxNumber";
+import AverageCalculator from './components/AverageCalculator';
+import ReplaceQuestionMarks from './components/ReplaceQuestionMarks';
+import FormUNJU from './components/FormUNJU';
+import CasillasActivity from './components/CasillasActivity';
+import PetRegistry from './components/PetRegistry';
+
+function App() {
+  return (
+    <>
+      {/* <Login/> */} 
+      {/*<SalarySimulator/> */}
+      {/*<MaxNumber/>*/}
+      {/*<AverageCalculator/>*/}
+      {/*<ReplaceQuestionMarks/>*/}
+      {/*<FormUNJU/>*/}
+      {/*<CasillasActivity/>*/}
+      {/*<PetRegistry/>*/}
+    </>
+  )
+}
+
+export default App;
